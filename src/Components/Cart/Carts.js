54bfbcac087a782898abf445187495b0ba666ae0
HarunPHero <harun7309@gmail.com>
2022-12-12 18:16:06 +0600

    leatest commit

--- src/Components/Cart/Carts.js
+++ src/Components/Cart/Carts.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
